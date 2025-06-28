0eec8dbff692af56d98c390c2cf80f4c20a99e6a
surachetBKC <148043101+surachetBKC@users.noreply.github.com>
2025-06-28 10:27:38 +0700

    Initial game upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1751080338,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bggame-sheet0.png",
+		"images/jigsaw-sheet0.png",
+		"images/pic-sheet0.png",
+		"images/canvas.png",
+		"images/particles2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
